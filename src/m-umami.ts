804740f7e07be5d6c167fca05e3766ecc1fea0db
Clonescody <clonescody@protonmail.com>
2023-02-23 18:52:33 -0800

    move ethdistribution handler to marinate contract for more consistency with safe module

--- src/m-umami.ts
+++ src/m-umami.ts
@@ -2,6 +2,7 @@ import { BigDecimal } from "@graphprotocol/graph-ts";
 import { cmUMAMI } from "../generated/cmUMAMI/cmUMAMI";
 import {
   mUMAMI,
+  RewardAdded,
   RewardClaimed as RewardsClaimedEvent,
   Stake as MarinateStake,
   Transfer as MarinateTransfer,
@@ -12,6 +13,7 @@ import {
   SupplyBreakdown,
   UserBalanceTotal,
   RewardsClaim,
+  EthDistribution,
 } from "../generated/schema";
 import { sUMAMI } from "../generated/sUMAMI/sUMAMI";
 import { UMAMI } from "../generated/UMAMI/UMAMI";
@@ -263,4 +265,80 @@ export function handleRewardsClaimed(event: RewardsClaimedEvent): void {
   rewardsClaimed.user = event.params.staker.toHexString();
   rewardsClaimed.rewards = event.params.amount;
   rewardsClaimed.save();
+}
+
+export function handleRewardsAdded(event: RewardAdded): void {
+  let ethDistribution = new EthDistribution(event.transaction.hash.toHex());
+  ethDistribution.block = event.block.number;
+  ethDistribution.timestamp = event.block.timestamp;
+  ethDistribution.txHash = event.transaction.hash.toHex();
+  ethDistribution.ethDistributed = event.params.amount
+    .toBigDecimal()
+    .div(BigDecimal.fromString("1e18"));
+  ethDistribution.save();
+
+  let supplyBreakdown = new SupplyBreakdown(event.transaction.hash.toHex());
+  supplyBreakdown.event = "reward-added";
+  const UMAMIContract = UMAMI.bind(UMAMI_ADDRESS);
+  const sUMAMIContract = sUMAMI.bind(S_UMAMI_ADDRESS);
+  const mUMAMIContract = mUMAMI.bind(M_UMAMI_ADDRESS);
+  const cmUMAMIContract = cmUMAMI.bind(CM_UMAMI_ADDRESS);
+
+  const totalSupplyCallResult = UMAMIContract.try_totalSupply();
+  const umamiDecimalsCallResult = UMAMIContract.try_decimals();
+  const marinatingCallResult = mUMAMIContract.try_totalStaked();
+  const mUmamiDecimalsCallResult = mUMAMIContract.try_decimals();
+  const compoundingCallResult = cmUMAMIContract.try_totalDeposits();
+  const cmUmamiDecimalsCallResult = cmUMAMIContract.try_decimals();
+  const sUmamiDecimalsCallResult = sUMAMIContract.try_decimals();
+
+  let foreverLocked = BigDecimal.zero();
+  if (!umamiDecimalsCallResult.reverted && !sUmamiDecimalsCallResult.reverted) {
+    for (let i = 0; i < FORVER_LOCKED_HOLDERS.length - 1; i += 1) {
+      const lockedBalanceOfUMAMI = UMAMIContract.balanceOf(
+        FORVER_LOCKED_HOLDERS[i]
+      );
+      const lockedBalanceOfsUMAMI = sUMAMIContract.balanceOf(
+        FORVER_LOCKED_HOLDERS[i]
+      );
+      foreverLocked = foreverLocked.plus(
+        lockedBalanceOfUMAMI
+          .toBigDecimal()
+          .div(BigDecimal.fromString(`1e${umamiDecimalsCallResult.value}`))
+          .plus(
+            lockedBalanceOfsUMAMI
+              .toBigDecimal()
+              .div(BigDecimal.fromString(`1e${sUmamiDecimalsCallResult.value}`))
+          )
+      );
+    }
+  }
+  supplyBreakdown.foreverLocked = foreverLocked;
+
+  if (totalSupplyCallResult.reverted) {
+    supplyBreakdown.totalSupply = BigDecimal.zero();
+  } else {
+    supplyBreakdown.totalSupply = totalSupplyCallResult.value
+      .toBigDecimal()
+      .div(BigDecimal.fromString(`1e${umamiDecimalsCallResult.value}`));
+  }
+  if (marinatingCallResult.reverted) {
+    supplyBreakdown.marinating = BigDecimal.zero();
+  } else {
+    supplyBreakdown.marinating = marinatingCallResult.value
+      .toBigDecimal()
+      .div(BigDecimal.fromString(`1e${mUmamiDecimalsCallResult.value}`));
+  }
+  if (compoundingCallResult.reverted) {
+    supplyBreakdown.compounding = BigDecimal.zero();
+  } else {
+    supplyBreakdown.compounding = compoundingCallResult.value
+      .toBigDecimal()
+      .div(BigDecimal.fromString(`1e${cmUmamiDecimalsCallResult.value}`));
+  }
+
+  supplyBreakdown.timestamp = event.block.timestamp;
+  supplyBreakdown.block = event.block.number;
+  supplyBreakdown.txHash = event.transaction.hash.toHex();
+  supplyBreakdown.save();
 }
